9c1fbbe47dd9f91e8dadd02e499f035fe22c61b7
znight1020 <znight1020@naver.com>
2023-08-01 14:14:43 +0800

    test: camera test

--- dofbot_pad_controll.py
+++ dofbot_pad_controll.py
@@ -8,17 +8,18 @@ from Arm_Lib import Arm_Device
 
 # 게임패드 데이터를 저장할 변수 선언
 gamepad_data = None
+flag = False;
 
-def on_connect(client, userdata, flags, rc):
+def socket_on_connect(client2, userdata, flags, rc):
+    print("socket 연결 성공, 결과 코드: " + str(rc))
+    # camera 데이터를 topic 구독
+    client2.subscribe("jetson/camera")
+    flag = True
+
+def on_connect(client1, userdata, flags, rc):
     print("연결 성공, 결과 코드: " + str(rc))
     # 게임패드 데이터를 topic 구독
-    client.subscribe("my/topic")
-
-def on_connect_socket(client, userdata, flags, rc):
-    print("socket 연결 성공, 결과 코드: " + str(rc))
-    # 게임패드 데이터를 topic 구독
-    client.subscribe("jetson/camera")
-
+    client1.subscribe("my/topic")
 
 def on_message(client, userdata, msg):
     global gamepad_data
@@ -30,18 +31,15 @@ def on_message(client, userdata, msg):
     except json.JSONDecodeError as e:
         print(f"JSON decode error: {e}")
 
-def publish_camera_data(client, camera_data):
-    client.publish("jetson/camera", camera_data, qos=1)
-
 def handle_camera():
+    print("handle_camera")
     image = cv2.VideoCapture(0)
     try:
         while True:
             ret, frame = image.read()
             if ret:
                 _, buffer = cv2.imencode('.jpg', frame)
-                jpg_base64 = base64.b64encode(buffer).decode()
-                publish_camera_data(client2, jpg_base64)
+                publish_camera_data(buffer.tobytes())
     except KeyboardInterrupt:
         print("카메라 처리 종료")
         image.release()
@@ -80,19 +78,40 @@ def handle_gamepad():
         # 임의의 시간 간격으로 반복 수행
         time.sleep(0.05)
 
+
+def publish_camera_data(camera_data):
+    client2.publish("jetson/camera", camera_data, qos=1)
+
 # MQTT 클라이언트 설정
+client2 = mqtt.Client(transport="websockets")
+try:
+    client2.connect("129.254.174.120", 9002, 60)
+except:
+    print("socket connect failed")
+
+client2.on_connect = socket_on_connect
+client2.on_message = on_message
+
+
 client1 = mqtt.Client()
-client1.connect("129.254.174.120", 1883, 60)
+try:
+    client1.connect("129.254.174.120", 1883, 60)
+except:
+    print("connect failed")
+
 client1.on_connect = on_connect
 client1.on_message = on_message
 
-client2 = mqtt.Client()
-client2.connect("129.254.174.120", 9002, 60)
-client2.on_connect_socket = on_connect_socket
 
 # 두 개의 쓰레드 생성 및 실행
-camera_thread = threading.Thread(target=handle_camera)
+
 gamepad_thread = threading.Thread(target=handle_gamepad)
 
-camera_thread.start()
+if(flag == True):
+    camera_thread = threading.Thread(target=handle_camera)
+else:
+    print("camera_thread not running")
+
+
 gamepad_thread.start()
+camera_thread.start()
