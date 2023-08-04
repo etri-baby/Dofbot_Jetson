56cb3f7c8777f60bb3fe49c41224e9275175893c
znight1020 <znight1020@naver.com>
2023-08-04 12:51:24 +0800

    feat: streaming camera data success

--- dofbot_camera_data_send.py
+++ dofbot_camera_data_send.py
@@ -7,16 +7,22 @@ import cv2
 client = mqtt.Client(transport="websockets")
 
 def on_connect(client, userdata, flags, rc):
-    print("connect success")
-    client.subscribe("my/topic")
+    if(rc == 0):
+        print("Connected to MQTT broker")
+        client.subscribe("my/topic")
+    else:
+        print("Failed to connect, code: ", rc)
+
 
 def Camera_Handle():
     client.connect("129.254.174.120", 9002, 60)
     client.on_connect = on_connect
+    if(client.on_connect_fail):
+        try_reconnect(client)
+    else:
+        camera(client)
     
-    client.loop_start()  # loop_forever() 대신 loop_start()로 변경
-
-    
+    client.loop_start()  # loop_forever()
 
 def camera(client):
     image = cv2.VideoCapture(0)
@@ -25,16 +31,11 @@ def camera(client):
     try:
         
         while True:
-            start = time.time()
             ret, frame = image.read()
             if ret:
                 _, buffer = cv2.imencode('.jpg', frame)
                 jpg_base64 = base64.b64encode(buffer).decode()
                 client.publish("my/topic", jpg_base64, qos=0)
-                end = time.time()
-                t = end - start
-                fps = 1/t
-                print(fps)
                 
     except KeyboardInterrupt:
         print("카메라 처리 종료")
@@ -47,12 +48,11 @@ def try_reconnect(client):
             client.connect("129.254.174.120", 9002, 60)
             client.loop_start()
         except ConnectionError:
-            print("Failed to reconnect. Retrying in 5 seconds...")
-            time.sleep(5)
+            print("Failed to reconnect. Retrying in 3 seconds...")
+            time.sleep(3)
 
 camera_thread = threading.Thread(target=Camera_Handle)
 camera_thread.start()
 
 try_reconnect(client)
-camera(client)
 
