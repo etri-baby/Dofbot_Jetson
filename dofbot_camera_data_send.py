ceaa7b9a413c7848826cfa70fc9c109675707ba1
현수 <znight1020@naver.com>
2023-08-04 11:19:35 +0900

    fix: socket reconnect

--- dofbot_camera_data_send.py
+++ dofbot_camera_data_send.py
@@ -40,7 +40,19 @@ def camera(client):
         print("카메라 처리 종료")
         image.release()
 
+def try_reconnect(client):
+    while not client.is_connected():
+        try:
+            print("Trying to reconnect...")
+            client.connect("129.254.174.120", 9002, 60)
+            client.loop_start()
+        except ConnectionError:
+            print("Failed to reconnect. Retrying in 5 seconds...")
+            time.sleep(5)
+
 camera_thread = threading.Thread(target=Camera_Handle)
 camera_thread.start()
 
+try_reconnect(client)
 camera(client)
+
